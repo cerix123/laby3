5ae1d27fca22b69c47bc0bd1b209742485e6b398
cerix123 <115454970+cerix123@users.noreply.github.com>
2024-04-08 13:11:16 +0200

    Add files via upload

---
+++ Lab3x.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <regex>
+#include <filesystem>
+#include <fstream>
+
+void countFiles(const std::string& path, const std::regex& pattern)
+{
+	int count = 0;
+	for (auto& entry : std::filesystem::directory_iterator(path))
+	{
+		if (entry.is_regular_file() && std::regex_match(entry.path().string(), pattern))
+		{
+			count++;
+		}
+	}
+	std::filesystem::path output_dir("output");
+	std::filesystem::create_directories(output_dir);
+	std::ofstream outfile(output_dir / "dll_count.txt");
+	outfile << "Liczba plikow DLL= " << count << std::endl;
+	outfile.close();
+}
+
+
+int main()
+{
+	std::string path = "C:\\Windows\\System32";
+	std::regex pattern(".*\\.dll$");
+	countFiles(path, pattern);
+	return 0;
+}
